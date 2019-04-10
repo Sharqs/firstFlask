1e02cffd899a8988ca45467dfe354b549b4c3e4d
Ricky Bartenslager <RickyBartenslager@gmail.com>
2019-04-10 12:10:42 -0700

    added calc functions ignored fs test added new cache to ignore

--- flask-greet-calc/calc/app.py
+++ flask-greet-calc/calc/app.py
@@ -1,1 +1,35 @@
+from flask import Flask
+import operations
+from flask import request
 # Put your app in here.
+
+app = Flask(__name__)
+
+
+@app.route("/add")
+def addition_route():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(operations.add(a, b))
+
+
+@app.route('/sub')
+def subtract_route():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(operations.sub(a, b))
+
+
+@app.route('/mult')
+def multiply_route():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(operations.mult(a, b))
+
+
+@app.route('/div')
+def division_route():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    return str(operations.div(a, b))
+
